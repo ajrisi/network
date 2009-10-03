891ba9297231be62ba207c4484dc3479d316fcc9
Adam Risi <ajrisi@gmail.com>
2009-10-03 18:48:59 -0400

    Adding the template files for UDP and TCP operations

---
+++ tcp.h
@@ -0,0 +1,16 @@
+/**
+ * @file   tcp.c
+ * @author Adam Risi <ajrisi@gmail.com>
+ * @date   Sat Oct  3 18:25:59 2009
+ * 
+ * @brief  This file contains the routines used for TCP operations
+ * 
+ * 
+ */
+
+#ifndef _TCP_H_
+#define _TCP_H_
+
+
+
+#endif /* _TCP_H_ */
---
+++ udp.h
@@ -0,0 +1,17 @@
+/**
+ * @file   udp.h
+ * @author Adam Risi <ajrisi@gmail.com>
+ * @date   Sat Oct  3 18:39:29 2009
+ * 
+ * @brief  This file contains the routines related to UDP networking
+ * 
+ * 
+ */
+
+
+#ifndef _UDP_H_
+#define _UDP_H_
+
+
+
+#endif /* _UDP_H_ */
