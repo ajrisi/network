1d5688f9e6bf80fa7f279c85f6b2b404fcdecbb8
Adam Risi <ajrisi@gmail.com>
2009-10-03 19:06:38 -0400

    socket operations files added, just stubs

---
+++ sock.h
@@ -0,0 +1,16 @@
+/**
+ * @file   sock.h
+ * @author Adam Risi <ajrisi@gmail.com>
+ * @date   Sat Oct  3 19:05:20 2009
+ * 
+ * @brief  This file contains generer socket routines
+ * 
+ * 
+ */
+
+#ifndef _SOCK_H_
+#define _SOCK_H_
+
+
+
+#endif /* _SOCK_H_ */
